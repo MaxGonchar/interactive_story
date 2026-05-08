cf08cde4e7fa9b50c07751c153f720b5e111bc15
MaxGonchar <boborodada@gmail.com>
2026-05-09 00:10:56 +0300

    017: add SceneList presentational component

---
+++ frontend/src/components/SceneList.jsx
@@ -0,0 +1,19 @@
+function SceneList({ scenes = [], activeSceneId, onSelect }) {
+  if (scenes.length === 0) {
+    return <p>No scenes available</p>
+  }
+
+  return (
+    <ul>
+      {scenes.map((scene) => (
+        <li key={scene.id} onClick={() => onSelect(scene.id)}>
+          Scene {scene.id}
+          {scene.id === activeSceneId && ' (active)'}
+          {scene.finished && ' (finished)'}
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+export default SceneList
